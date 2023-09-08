4ec9841f7dac77211a5df036559533203428b2f8
MHanifPratama <mhanifp27@gmail.com>
2023-09-08 08:32:46 +0700

    Add Routes

---
+++ app/Views/profile_url.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th><?=$nama?></th>
+        </tr>
+        <tr>
+            <td>NPM</td>
+            <td><?=$npm?></td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td><?=$alamat?></td>
+        </tr>
+    </table>
+</body>
+</html>
